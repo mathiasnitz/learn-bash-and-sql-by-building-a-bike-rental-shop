0cce64006ddc8db9ab0efda8d03bdb9e807f0738
matt n <mathiasnitz@gmx.de>
2025-03-24 09:40:30 +0000

    Save progress: 1270.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+
 PSQL="psql -X --username=freecodecamp --dbname=bikes --tuples-only -c"
 
 echo -e "\n~~~~~ Bike Rental Shop ~~~~~\n"
-
 
 MAIN_MENU() {
   if [[ $1 ]]
   then
     echo -e "\n$1"
   fi
-  echo How may I help you?
+
+  echo "How may I help you?" 
   echo -e "\n1. Rent a bike\n2. Return a bike\n3. Exit"
   read MAIN_MENU_SELECTION
 
@@ -22,38 +23,39 @@ MAIN_MENU() {
 }
 
 RENT_MENU() {
-  #get available bikes
-
+  # get available bikes
   AVAILABLE_BIKES=$($PSQL "SELECT bike_id, type, size FROM bikes WHERE available = true ORDER BY bike_id")
 
-  #if no bikes available
+  # if no bikes available
   if [[ -z $AVAILABLE_BIKES ]]
   then
-    #send to main menu
+    # send to main menu
     MAIN_MENU "Sorry, we don't have any bikes available right now."
   else
-    #display available bikes
+    # display available bikes
     echo -e "\nHere are the bikes we have available:"
     echo "$AVAILABLE_BIKES" | while read BIKE_ID BAR TYPE BAR SIZE
     do
       echo "$BIKE_ID) $SIZE\" $TYPE Bike"
     done
+
     # ask for bike to rent
     echo -e "\nWhich one would you like to rent?"
     read BIKE_ID_TO_RENT
+
     # if input is not a number
     if [[ ! $BIKE_ID_TO_RENT =~ ^[0-9]+$ ]]
     then
       # send to main menu
       MAIN_MENU "That is not a valid bike number."
     else
-      #get bike availability
-      BIKE_AVAILABILITY=$($PSQL "SELECT available FROM bikes WHERE bike_id=$BIKE_ID_TO_RENT AND available = TRUE")
-      
-      #if not available
+      # get bike availability
+      BIKE_AVAILABILITY=$($PSQL "SELECT available FROM bikes WHERE bike_id = $BIKE_ID_TO_RENT AND available = true")
+
+      # if not available
       if [[ -z $BIKE_AVAILABILITY ]]
       then
-        #send to main menu
+        # send to main menu
         MAIN_MENU "That bike is not available."
       else
         # get customer info
@@ -63,24 +65,21 @@ RENT_MENU() {
         CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone = '$PHONE_NUMBER'")
 
         # if customer doesn't exist
-
         if [[ -z $CUSTOMER_NAME ]]
         then
           # get new customer name
+          echo -e "\nWhat's your name?"
 
           # insert new customer
 
         fi
       fi
-      
     fi
-    
   fi
-  
 }
 
 RETURN_MENU() {
-  echo Return Menu
+  echo "Return Menu"
 }
 
 EXIT() {
